029d0b334576f988b3448b262f49858c596c3f36
Amna Ali <98670206+Amna-28@users.noreply.github.com>
2022-10-09 00:25:43 +0200

    Update main.py

--- model/framework/grover/main.py
+++ model/framework/grover/main.py
@@ -6,6 +6,7 @@ import torch
 import pandas as pd
 from rdkit import RDLogger
 from pathlib import Path
+import tempfile
 
 from grover.util.parsing import parse_args, get_newest_train_args
 from grover.util.utils import create_logger
@@ -52,7 +53,8 @@ def smiles_to_dataframe(txt_file_path):
     
     return input_csv_path
 
-    
+tmp_folder = tempfile.mktemp()
+features_path = os.path.join(tmp_folder, "features.npz")    
 
 if __name__ == '__main__':
     # setup random seed
@@ -74,11 +76,8 @@ if __name__ == '__main__':
     p = Path(s)
     model_path = str(p.parent.parent.absolute())
 
-    args = Namespace(batch_size=32, checkpoint_dir=model_path+'/framework/finetune/toxcast', checkpoint_path=None, checkpoint_paths=[model_path+'/framework/finetune/toxcast/fold_0/model_0/model.pt', model_path+'/framework/finetune/toxcast/fold_2/model_0/model.pt', model_path+'/framework/finetune/toxcast/fold_1/model_0/model.pt'], cuda=False, data_path=csv_path, ensemble_size=3, features_generator=None, features_path=['features.npz'], fingerprint=False, gpu=0, no_cache=True, no_features_scaling=True, output_path=output_path, parser_name='predict')
-
-
-    sf.save_features_main(csv_path)
-
+    args = Namespace(batch_size=32, checkpoint_dir=model_path+'/framework/finetune/toxcast', checkpoint_path=None, checkpoint_paths=[model_path+'/framework/finetune/toxcast/fold_0/model_0/model.pt', model_path+'/framework/finetune/toxcast/fold_2/model_0/model.pt', model_path+'/framework/finetune/toxcast/fold_1/model_0/model.pt'], cuda=False, data_path=csv_path, ensemble_size=3, features_generator=None, features_path=[features_path], fingerprint=False, gpu=0, no_cache=True, no_features_scaling=True, output_path=output_path, parser_name='predict')
+    sf.save_features_main(csv_path,features_path)
     train_args = get_newest_train_args()
     avg_preds, test_smiles = make_predictions(args, train_args)
     write_prediction(avg_preds, test_smiles, args)
