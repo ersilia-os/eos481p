aee3b30f2828a6c13b9a34523a3bbb2066f35b57
Zakia Yahya <zyahya.msee15seecs@seecs.edu.pk>
2023-07-20 23:39:49 +0500

    first major commit

--- src/service.py
+++ src/service.py
@@ -43,7 +43,7 @@ class Model(object):
     def set_framework_dir(self, dest):
         self.framework_dir = os.path.abspath(dest)
 
-    def predict(self, smiles_list):
+    def run(self, smiles_list):
         tmp_folder = tempfile.mkdtemp(prefix="eos-")
         data_file = os.path.join(tmp_folder, self.DATA_FILE)
         pred_file = os.path.join(tmp_folder, self.PRED_FILE)
@@ -53,7 +53,7 @@ class Model(object):
                 f.write(smiles + os.linesep)
         run_file = os.path.join(tmp_folder, self.RUN_FILE)
         with open(run_file, "w") as f:
-            lines = ["python {0}/grover/main.py {1} {2}".format(self.framework_dir, data_file, pred_file)] 
+            lines = ["bash {0}/run.sh {0} {1} {2}".format(self.framework_dir, data_file, pred_file)] 
             f.write(os.linesep.join(lines))
         cmd = "bash {0}".format(run_file)
         with open(log_file, "w") as fp:
@@ -127,8 +127,8 @@ class Artifact(BentoServiceArtifact):
 @artifacts([Artifact("model")])
 class Service(BentoService):
     @api(input=JsonInput(), batch=True)
-    def predict(self, input: List[JsonSerializable]):
+    def run(self, input: List[JsonSerializable]):
         input = input[0]
         smiles_list = [inp["input"] for inp in input]
-        output = self.artifacts.model.predict(smiles_list)
+        output = self.artifacts.model.run(smiles_list)
         return [output]
